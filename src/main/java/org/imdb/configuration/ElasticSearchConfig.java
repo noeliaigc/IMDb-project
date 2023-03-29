dbf0bdfe936ed17ce66c30b4cbce198a0c0ab012
Noelia Iglesias Cuesta <98741282+noeliaigc@users.noreply.github.com>
2023-03-29 12:17:34 +0200

    Indexing and cerebro working

--- src/main/java/org/imdb/configuration/ElasticSearchConfig.java
+++ src/main/java/org/imdb/configuration/ElasticSearchConfig.java
@@ -20,7 +20,8 @@ public class ElasticSearchConfig {
     public ElasticsearchClient getElasticClient(){
         // Create the low-level client
         RestClient restClient = RestClient.builder(
-                new HttpHost("localhost", 9200)).build();
+                new HttpHost("localhost", 9200),
+                new HttpHost("elasticsearch", 9200)).build();
 
         // Create the transport with a Jackson mapper
         ElasticsearchTransport transport = new RestClientTransport(
